4f505a3f7cdd269c2f4c5b2d21acfc301735108c
imtheraf <imtheraf@gmail.com>
2024-12-24 18:48:15 +0600

    Bind Flask to 0.0.0.0 and use dynamic port

--- download.py
+++ download.py
@@ -118,4 +118,5 @@ def download():
     return jsonify({"url"  : download_url, "title" : video_title})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 locally if PORT is not set
+    app.run(debug=True, host="0.0.0.0", port=port)
